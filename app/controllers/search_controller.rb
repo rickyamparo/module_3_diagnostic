7cb58e33f810c60da86ca872c127616debd8e2b6
Ricky Amparo <rickyamparo@gmail.com>
2017-10-18 10:14:40 -0600

    used postman to get connection to start working, will now implement into search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,9 @@
 class SearchController < ApplicationController
   def index
+    @conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/') do |faraday|
+      faraday.adapater Faraday.default
 
+    end
+    binding.pry
   end
 end
