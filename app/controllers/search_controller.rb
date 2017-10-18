3792852e27d6f665f25c5ebbe744a176edaebb24
Ricky Amparo <rickyamparo@gmail.com>
2017-10-18 10:29:53 -0600

    complete the faraday connection, trouble shooting get requests from api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.new(:url => 'https://api.data.gov/nrel/alt-fuel-stations/') do |faraday|
-      faraday.adapater Faraday.default
-
+    @conn = Faraday.new(:url => "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV["X-API-KEY"]}") do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.headers["X-API-KEY"] = ENV["X-API-KEY"]
     end
     binding.pry
   end
